509d2060ebe6d7a5db439b9920e59d70916021a3
Samuel Johnson Stoever <samuel.johnson.stoever@gmail.com>
2012-11-01 21:33:17 -0400

    Fully working publish.sh

--- publish.sh
+++ publish.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-#. ./config.sh
-dnbpath="."
-mdparser="multimarkdown"
+# Configuration options
+dnbpath="." #This should be changed if calling this script from outside its parent dir.
+outputpath="output"
+#mdparser="multimarkdown"
+mdparser="/Volumes/s_fs/samsHome/packages/multiMarkdown/bin/multimarkdown"
+
+cd $dnbpath
 
 # HTML ouputting functions
 openDiv () {
@@ -11,24 +15,49 @@ openDiv () {
     else 
 	echo "<div id=\""$1"\">"
     fi
-    
 }
 endDiv () {
     echo '</div>'
 }
 gethead1 () {
-    $mdparser $dnbpath/site/head1.md | sed 's/<p>/\t/g' | sed 's/<\/p>/\t/g'
+    $mdparser site/head1.md | sed 's/<p>//g' | sed 's/<\/p>//g'
 }
 gethead2 () {
-    $mdparser $dnbpath/site/head2.md | sed 's/<p>/\t/g' | sed 's/<\/p>/\t/g'
+    $mdparser site/head2.md | sed 's/<p>//g' | sed 's/<\/p>//g'
 }
 newline () {
     echo ' '
 }
 printCredit () {
     openDiv credit
-    $mdparser $dnbpath/site/credit.md | sed 's/<p>/\t/g' | sed 's/<\/p>/\t/g'
+    $mdparser site/credit.md | sed 's/<p>//g' | sed 's/<\/p>//g'
     endDiv
+}
+printEntry () {
+	entryfile=$1
+	# Open Entry ID Number
+	openDiv $(echo $entryfile | cut -d/ -f 2 | cut -d. -f 1)
+	# Open topper
+	openDiv topper
+	sed -n '2p' $entryfile | sed 's/Date://g' | $mdparser | sed 's/<p>//g' | sed 's/<\/p>//g'
+	# Close topper
+	endDiv
+	echo '<h2>'
+	sed -n '1p' $entryfile | sed 's/Title://g' | $mdparser | sed 's/<p>//g' | sed 's/<\/p>//g'
+	echo '</h2>'
+	# Open entry
+	echo "<div class=\"entry\">"
+	tail -n+4 $entryfile | $mdparser
+	# Close entry
+	endDiv
+	# Close Entry ID Number
+	endDiv
+}
+printEntryLink () {
+	entryfile=$1
+	echo "<li><a href=\"#"$(echo $entryfile | cut -d/ -f 2 | cut -d. -f 1)"\">"
+	sed -n '1p' $entryfile | sed 's/Title://g' | $mdparser | sed 's/<p>//g' | sed 's/<\/p>//g'
+	echo "</a></li>"
 }
 #HTML Sections
 printHead () {
@@ -67,15 +96,51 @@ printEnd () {
 printEntries () {
     #Open content
     openDiv content
-    
+    entryFILES=$(ls entries/*.md | tail -r )
+    for f in $entryFILES
+    do
+    	printEntry $f
+    done
+    #Open/Close navigation
+    echo "<div class=\"navigation\"></div>"
     #Close content
     endDiv
 }
+printSidebar () {
+	#Open sidebar
+	openDiv sidebar
+	echo '<ul>'
+	echo '<li>Entries:</li>'
+	echo '<ul>'
+	entryFILES=entries/*.md
+    for f in $entryFILES
+    do
+    	printEntryLink $f
+    done
+    echo '</ul>'
+	echo '<li>Blog Links:</li>'
+	$mdparser site/blogLinks.md
+	echo '</ul>'
+	#Close sidebar
+	endDiv
+}
+# The structure of the entire document
 printHTML () {
     printHead
     printEntries
+    printSidebar
     printCredit
     printEnd
 }
 
-printHTML > index.html
+### Main Body of the Script
+if [ -d $outputpath.bak ]; then
+	rm -r $outputpath.bak
+fi
+mv $outputpath $outputpath.bak
+mkdir $outputpath
+if [ -d entries/images ]; then
+	cp -r entries/images $outputpath/
+fi
+cp -r css $outputpath/
+printHTML > $outputpath/index.html
